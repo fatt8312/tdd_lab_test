b8ae3613807bbca15af8c37e7991deb15e9b550f
fatt8312 <115389231+fatt8312@users.noreply.github.com>
2023-03-22 13:51:27 +0700

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World1"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+# @app.get("/hello/{name}")
+# def read_name(name: str = None):
+#     return {"hello": name}
+
+@app.get("/callname/{name}")
+def call_name(name: str):
+    return {"hello": name}
+
+@app.post("/callname/{name}")
+def call_name(name: str):
+    return {"hello": name}
+
+handler = Mangum(app)
---
+++ app/tests/test_main.py
@@ -0,0 +1,28 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+# def test_read_main():
+#     response = client.get("/")
+#     assert response.status_code == 200
+#     assert response.json() == {"Hello": "World1"}
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World1"}
+
+def test_callname():
+    name = "Alice"
+    response = client.get(f"/callname/{name}")
+    assert response.status_code == 200
+    assert response.json() == {"hello": name}
+
+def test_callname():
+    name = "Hakim"
+    response = client.post("/callname", json={"name": name})
+    assert response.status_code == 200
+    assert response.json() == {"hello": name}
