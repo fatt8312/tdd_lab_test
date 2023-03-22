17d4e6f82a9349b61706c0fbe81fe6dd5b7fc2aa
fatt8312 <115389231+fatt8312@users.noreply.github.com>
2023-03-22 14:13:11 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,23 +5,19 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World1"}
+    return {"callname": "World1"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
-# @app.get("/hello/{name}")
-# def read_name(name: str = None):
-#     return {"hello": name}
+@app.get("/hello/{name}")
+def read_name(name: str = None):
+    return {"callname": name}
 
-@app.get("/callname/{name}")
+@app.post("/callname/{name}")
 def call_name(name: str):
-    return {"hello": name}
-
-@app.route('/callname', methods=['POST'])
-def call_name(name: str):
-    return {"hello": name}
+    return {"callname": name}
 
 handler = Mangum(app)
