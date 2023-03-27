085af1ce2f9048ee286ddf470af4268929eae96e
fatt8312 <115389231+fatt8312@users.noreply.github.com>
2023-03-27 10:52:24 +0700

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -4,17 +4,42 @@ from app.main import app
 
 client = TestClient(app)
 
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"Hello": "World1"}
+def test_calculate_monthly_payment():
+    # Test case 1: Minimum down payment, no interest
+    car_price = 100000
+    down_payment = 20000
+    monthly_payment = calculate_monthly_payment(car_price, down_payment)
+    assert monthly_payment == 6666.67
 
-def test_callname():
-    response = client.get("/callname/pajjaree")
-    assert response.status_code == 200
-    assert response.json() == {"hello": "pajjaree"}
-    
-def test_callname():
-    response = client.post("/callname", data={"name": "pajjaree"})
-    assert response.status_code == 200
-    assert response.json() == {"hello": "pajjaree"}
+    # Test case 2: Minimum down payment, with interest
+    car_price = 80000
+    down_payment = 16000
+    interest_rate = 0.05
+    monthly_payment = calculate_monthly_payment(car_price, down_payment, interest_rate)
+    assert monthly_payment == 5786.67
+
+    # Test case 3: Down payment less than 20%
+    car_price = 90000
+    down_payment = 15000
+    with pytest.raises(ValueError):
+        monthly_payment = calculate_monthly_payment(car_price, down_payment)
+
+def test_calculate_total_payment():
+    # Test case 1: Minimum down payment, no interest
+    car_price = 100000
+    down_payment = 20000
+    total_payment = calculate_total_payment(car_price, down_payment)
+    assert total_payment == 100000
+
+    # Test case 2: Minimum down payment, with interest
+    car_price = 80000
+    down_payment = 16000
+    interest_rate = 0.05
+    total_payment = calculate_total_payment(car_price, down_payment, interest_rate)
+    assert total_payment == 84000
+
+    # Test case 3: Down payment less than 20%
+    car_price = 90000
+    down_payment = 15000
+    with pytest.raises(ValueError):
+        total_payment = calculate_total_payment(car_price, down_payment)
